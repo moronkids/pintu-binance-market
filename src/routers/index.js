ddf95be6632271f3afed76ad29f7a47876270760
Sulistyo <sulistyoardani@gmail.com>
2021-11-07 22:10:08 +0700

    fix: routes

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import GuestRoutes from "routers/guest";
 import LoggedRoutes from "routers/logged";
-import Agent from "components/dashboard";
+import Dashboard from "components/dashboard";
 import NotFound from "components/not_found_page";
 
 const Routes = () => {
@@ -10,13 +10,7 @@ const Routes = () => {
     <>
       <Switch>
         {/* logged routes */}
-        <LoggedRoutes exact path="/home" component={Agent} />
-        <LoggedRoutes exact path="/" component={Agent} />
-
-        {/* logged routes */}
-        {/* guest routes */}
-        {/* <GuestRoutes exact path="/socket" component={Socket} /> */}
-        {/* guest routes */}
+        <LoggedRoutes exact path="/" component={Dashboard} />
         <Route component={NotFound} />
       </Switch>
     </>
